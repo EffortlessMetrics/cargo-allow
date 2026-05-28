0a31e017a9584f50fd63ba4616372c9a3e29333f
Steven Zimmerman, CPA <15812269+EffortlessSteven@users.noreply.github.com>
2026-05-28 09:58:28 -0400

    legacy: split process network converters

--- crates/allow-policy-legacy/src/lib.rs
+++ crates/allow-policy-legacy/src/lib.rs
@@ -4,6 +4,7 @@ mod converter_exception_entries;
 mod converter_file_entries;
 mod converter_panic_entries;
 mod converter_policy_entries;
+mod converter_process_network_entries;
 mod converter_support;
 mod converter_workflow_entries;
 mod converters;
