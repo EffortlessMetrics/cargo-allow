167d2ba2e82cfcb862cb3fd5c597d6637473774a
Steven Zimmerman, CPA <15812269+EffortlessSteven@users.noreply.github.com>
2026-05-28 09:48:28 -0400

    legacy: split file converters (#382)

--- crates/allow-policy-legacy/src/lib.rs
+++ crates/allow-policy-legacy/src/lib.rs
@@ -1,6 +1,7 @@
 use allow_core::SimpleDate;
 
 mod converter_exception_entries;
+mod converter_file_entries;
 mod converter_panic_entries;
 mod converter_policy_entries;
 mod converter_source_entries;
