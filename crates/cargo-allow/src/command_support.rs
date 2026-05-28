7fe4eddbdb32f6a0400b2805617baa71aee719cb
Steven Zimmerman, CPA <15812269+EffortlessSteven@users.noreply.github.com>
2026-05-28 05:03:41 -0400

    cli: move command support exports (#334)

---
+++ crates/cargo-allow/src/command_support.rs
@@ -0,0 +1,24 @@
+#[cfg(test)]
+pub(crate) use crate::cli::{CargoAllowCli, CargoAllowCommand, normalized_args};
+pub(crate) use crate::cli_types::{InventoryFacts, OutputFormat, RootArgs};
+pub(crate) use crate::companion::{canonical_companion_findings, extend_unique_findings};
+pub(crate) use crate::compat::load_compat_world;
+pub(crate) use crate::io::{write_file, write_file_no_overwrite};
+pub(crate) use crate::kind_filter::{
+    FamilyFilter, KindFilter, is_clippy_compat_kind, is_dependency_surface_compat_kind,
+    is_executable_compat_kind, is_network_compat_kind, is_no_panic_allowlist_compat_kind,
+    is_panic_compat_kind, is_process_compat_kind, is_unsafe_compat_kind, is_workflow_compat_kind,
+    parse_kind_filter,
+};
+pub(crate) use crate::policy_config::{
+    config_path, git_relative_config_path, load_config_optional, load_config_required,
+    root_relative_path,
+};
+pub(crate) use crate::reporting::{
+    ReportRenderArgs, policy_baseline_debt_entries, print_report, report_config,
+};
+pub(crate) use crate::selector::selector_from_finding;
+pub(crate) use crate::source_tree::{
+    scope_has_wildcard, source_package_name, source_tree_path_matches_filter,
+};
+pub(crate) use crate::world::{load_world, load_world_with_evidence_validation};
